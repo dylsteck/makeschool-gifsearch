6a3ba93949a6e72056a838347697b141e446124c
Dylan Steck <dylancsteck@gmail.com>
2017-12-27 14:53:23 -0600

    working on giphy api

--- app.js
+++ app.js
@@ -2,16 +2,33 @@
 var express = require('express');
 var app = express();
 var exphbs  = require('express-handlebars');
+// require http module
+var http = require('http');
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
-app.get('/', function(req, res){
-	res.render('homepage', {});
-});
-app.get('/hello-gif', function (req, res) {
-  var gifUrl = 'http://media2.giphy.com/media/gYBVM1igrlzH2/giphy.gif';
-  res.render('hello-gif', {gifUrl: gifUrl});
+app.get('/', function (req, res) {
+  console.log(req.query.term);
+  var queryString = "funny cat";
+  // encode to remove whitespace + restricted characters
+  var term = encodeURIComponent(queryString);
+  // search put in giphy api search
+   var url = 'http://api.giphy.com/v1/gifs/search?q=' + term + '&api_key=dc6zaTOxFJmzC';
+
+   http.get(url, function(response) {
+	   	//set encoding response
+	   	response.setEncoding('utf8');
+
+	   	var body = '';
+
+	   	response.on('data', function(d) {
+	   		//when data fully received, parse
+	   		var paresed = JSON.parse(body);
+	   		//render home template with gifs
+	   		res.render('home', {gifs: parsed.data});
+		}); 
+   });
 });
 
 app.get('/greetings/:name', function (req, res) {
@@ -19,6 +36,7 @@ app.get('/greetings/:name', function (req, res) {
   res.render('greetings', {name: name});
 });
 
+//runs the app on port 3000
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
